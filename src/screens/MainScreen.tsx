da465b7f85f50532cbd5c4a0400b64b81a2fb759
gayeoniya <cindy20269405@gmail.com>
2026-07-01 21:42:20 +0900

    feat: 메인 사이드바 ui 수정

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -128,7 +128,7 @@ export default function MainScreen() {
 
       {user && (
         <div
-          className={`absolute top-0 right-0 h-full w-[350px] bg-white shadow-xl z-30 flex flex-col transform transition-transform duration-300 ease-in-out ${
+          className={`fixed top-0 right-0 h-screen w-[350px] bg-white shadow-xl z-30 flex flex-col transform transition-transform duration-300 ease-in-out ${
             isSidebarOpen ? "translate-x-0" : "translate-x-full"
           }`}
         >
